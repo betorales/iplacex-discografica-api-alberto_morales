1475448e1fd9e6cd2c7c365791e7cb0a1abe577d
betorales <g.moralesa@icloud.com>
2024-09-19 22:40:20 -0300

    Adding new content and minor changes

--- src/main/java/org/iplacex/proyectos/discografia/artistas/ArtistaController.java
+++ src/main/java/org/iplacex/proyectos/discografia/artistas/ArtistaController.java
@@ -1,5 +1,30 @@
 package org.iplacex.proyectos.discografia.artistas;
 
-public class ArtistaController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ArtistaController {
+    @Autowired
+    private IArtistaRepository artistaRepo;
+
+    @PostMapping(
+        value = "/artista",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+
+    public ResponseEntity<Artista> HandleInsertArtistaRequest(@RequestBody Artista artista){
+        Artista temp = artistaRepo.insert(artista);
+        return new ResponseEntity<>(temp, null, HttpStatus.CREATED);
+    }
 }
--- src/main/java/org/iplacex/proyectos/discografia/discos/DiscoController.java
+++ src/main/java/org/iplacex/proyectos/discografia/discos/DiscoController.java
@@ -1,5 +1,14 @@
 package org.iplacex.proyectos.discografia.discos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
 public class DiscoController {
-    
+    @Autowired
+    private IDiscoRepository artistaRepo;
 }
