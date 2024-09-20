1049d166de95c6369ed32688306d1b1903a894b7
betorales <g.moralesa@icloud.com>
2024-09-20 18:32:21 -0300

    Adding new conten and fixing some errors

--- src/main/java/org/iplacex/proyectos/discografia/discos/DiscoController.java
+++ src/main/java/org/iplacex/proyectos/discografia/discos/DiscoController.java
@@ -1,7 +1,18 @@
 package org.iplacex.proyectos.discografia.discos;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.iplacex.proyectos.discografia.artistas.IArtistaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +21,51 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class DiscoController {
     @Autowired
-    private IDiscoRepository artistaRepo;
+    private IDiscoRepository discoRepo;
+    private IArtistaRepository artistaRepo;
+
+    // Añadir - POST
+    @PostMapping(
+        value = "/disco",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Disco> HandlePostDiscoRequest(@RequestBody Disco disco){
+        Disco temp = discoRepo.insert(disco);
+        return new ResponseEntity<>(temp, null, HttpStatus.CREATED);
+    }
+
+    // Obtener todos los discos - GET
+    @GetMapping(
+        value = "/discos",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<Disco>> HandleGetDiscosRequest(){
+        List<Disco> discos = discoRepo.findAll();
+        return new ResponseEntity<>(discos, null, HttpStatus.OK);
+    }
+
+    // Obtener un disco por ID - GET
+    @GetMapping(
+        value = "/disco/{id}",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Disco> HandleGetDiscoRequest(@PathVariable("id") String id){
+        Optional<Disco> temp = discoRepo.findById(id);
+        if(!temp.isPresent()){
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(temp.get(), null, HttpStatus.OK);
+    }
+
+    // Obtener los discos por artista - GET
+    @GetMapping(
+        value = "/artista/{id}/discos",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Disco> HandleGetDiscosByArtistaRequest(){
+
+
+        return
+    }
 }
